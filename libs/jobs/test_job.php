acf53348c2f3e98f7850857e7979eb0933fd977d
Jose Diaz-Gonzalez <myaccounts@savant.be>
2011-02-09 00:27:19 -0500

    Added extra examples to the included TestJob class and readme for CakeJob::loadModel() and CakeJob::loadComponent()

--- libs/jobs/test_job.php
+++ libs/jobs/test_job.php
@@ -11,6 +11,20 @@ class CakeDjjob_TestJob extends CakeJob {
 
         $count = $this->Job->find('count');
         $this->out(sprintf('There are %s jobs in the queue', $count));
+
+        $this->out('Sending an email');
+        $this->loadComponent('Email');
+        $this->Email->_set(array(
+            'to' => 'postmaster@localhost',
+            'from' => 'noreply@example.com',
+            'subject' => 'Cake SMTP test',
+            'replyTo' => 'noreply@example.com'
+            'template' => null,
+            'sendAs' => 'both',
+        ));
+        $this->Email->send('This is the body of the message');
+        $this->out('Email sent');
+
         $this->out("Job completed");
     }
 
