2924a1dcff8a5bd16c2bd3be2b6a1bcddbf21a7c
Jose Diaz-Gonzalez <myaccounts@savant.be>
2011-02-09 00:17:42 -0500

    Dummy Controller and AppController for plugin

---
+++ cake_djjob_app_controller.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * CakeDjjob Application level Controller
+ *
+ * This file is plugin-wide controller file. You can put all
+ * plugin-wide controller-related methods here.
+ *
+ * PHP versions 4 and 5
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the below copyright notice.
+ *
+ * @copyright     Copyright 2010, Jose Diaz-Gonzalez. (http://josediazgonzalez.com)
+ * @link          http://github.com/josegonzalez/cake_djjob
+ * @package       cake_djjob
+ * @subpackage    cake_djjob.controller
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+class CakeDjjobAppController extends AppController {
+}
---
+++ controllers/cake_djjob_dummies_controller.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * CakeDjjobDummiesController class.
+ *
+ * PHP versions 4 and 5
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the below copyright notice.
+ *
+ * @copyright     Copyright 2010, Jose Diaz-Gonzalez. (http://josediazgonzalez.com)
+ * @link          http://github.com/josegonzalez/cake_djjob
+ * @package       cake_djjob
+ * @subpackage    cake_djjob.controller
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+class CakeDjjobDummiesController extends CakeDjjobAppController {
+
+/**
+ * The name of this controller. Controller names are plural, named after the model they manipulate.
+ *
+ * @var string
+ * @access public
+ * @link http://book.cakephp.org/view/959/Controller-Attributes
+ */
+    var $name = 'CakeDjjobDummies';
+
+/**
+ * An array containing the class names of models this controller uses.
+ *
+ * Example: `var $uses = array('Product', 'Post', 'Comment');`
+ *
+ * Can be set to array() to use no models.  Can be set to false to
+ * use no models and prevent the merging of $uses with AppController
+ *
+ * @var mixed A single name as a string or a list of names as an array.
+ * @access protected
+ * @link http://book.cakephp.org/view/961/components-helpers-and-uses
+ */
+    var $uses = array();
+
+}
