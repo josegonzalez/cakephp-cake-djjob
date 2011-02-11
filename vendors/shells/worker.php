a118fc9663012a3c1da62a19eece2024eaa6d52b
Jose Diaz-Gonzalez <myaccounts@savant.be>
2011-02-11 01:27:08 -0500

    added 'status' and 'clean' methods to worker, as well as updating docblocks for the new methods

--- vendors/shells/worker.php
+++ vendors/shells/worker.php
@@ -1,11 +1,17 @@
 <?php
-App::import('Model', 'ConnectionManager');
-App::import('Lib', 'CakeDjjob.cake_job', array(
-    'file' => 'jobs' . DS . 'cake_job.php',
-));
-App::import('Vendor', 'Djjob.DJJob', array(
-    'file' => 'DJJob.php',
-));
+if (!class_exists('ConnectionManager')) {
+    App::import('Model', 'ConnectionManager');
+}
+if (!class_exists('CakeJob')) {
+    App::import('Lib', 'CakeDjjob.cake_job', array(
+        'file' => 'jobs' . DS . 'cake_job.php',
+    ));
+}
+if (!class_exists('DJJob')) {
+    App::import('Vendor', 'Djjob.DJJob', array(
+        'file' => 'DJJob.php',
+    ));
+}
 
 /**
  * Uses _ (underscore) to denote plugins, since php classnames
@@ -44,11 +50,15 @@ class WorkerShell extends Shell {
         'type'      => 'mysql',
     );
 
-    var $_queue = 'default';
+    var $_queue = null;
     var $_count = 0;
     var $_sleep = 5;
     var $_max   = 5;
     var $_debug = 0;
+    var $_action = 0;
+    var $_column = 0;
+    var $_date = null;
+    var $_save = true;
 
 /**
  * Override initialize
@@ -89,11 +99,41 @@ class WorkerShell extends Shell {
             $this->_debug   = (int) $this->params['debug'];
         }
 
+        if (!empty($this->params['save'])) {
+            if ($this->params['save'] == 'false') {
+                $this->params['save'] = 0;
+            }
+            if ($this->params['save'] == 'true') {
+                $this->params['save'] = 1;
+            }
+            $this->_save   = (int) $this->params['save'];
+        }
         if (!empty($this->params['connection'])) {
             $this->settings['connection'] = $this->params['connection'];
         }
         if (!empty($this->params['type'])) {
             $this->settings['type'] = $this->params['type'];
+        }
+
+        if (!empty($this->params['action'])) {
+            if (!in_array($this->params['action'], array('clean', 'unlock'))) {
+                $this->cakeError('error', array(array(
+                    'code' => '', 'name' => '',
+                    'message' => 'Invalid action ' . $this->params['action']
+                )));
+            }
+            $this->_action  = $this->params['action'];
+            if ($this->_action == 'clean') {
+                $this->_column = 'failed_at';
+            } elseif ($this->_action == 'unlock') {
+                $this->_column = 'locked_at';
+            }
+        }
+
+        if (empty($this->params['date'])) {
+            $this->_date = date('Y-m-d H:i:s');
+        } else {
+            $this->_date = date('Y-m-d H:i:s', strtotime($this->params['date']));
         }
 
         ini_set('unserialize_callback_func', 'unserialize_jobs');
@@ -133,6 +173,13 @@ class WorkerShell extends Shell {
 
     function run() {
         Configure::write('debug', $this->_debug);
+        if (empty($this->_queue)) {
+            $this->cakeError('error', array(array(
+                'code' => '', 'name' => '',
+                'message' => 'No queue set'
+            )));
+        }
+
         $worker = new DJWorker(array(
             "queue" => $this->_queue,
             "count" => $this->_count,
@@ -140,6 +187,57 @@ class WorkerShell extends Shell {
             "max_attempts" => $this->_max,
         ));
         $worker->start();
+    }
+
+    function status() {
+        Configure::write('debug', $this->_debug);
+        if (empty($this->_queue)) {
+            $this->cakeError('error', array(array(
+                'code' => '', 'name' => '',
+                'message' => 'No queue set'
+            )));
+        }
+
+        $status = DJJob::status($this->_queue);
+        foreach ($status as $name => $count) {
+            $this->out(sprintf("%s Jobs: %d", Inflector::humanize($name), $count));
+        }
+    }
+
+    function cleanup() {
+        Configure::write('debug', $this->_debug);
+        if (empty($this->_column)) {
+            $this->cakeError('error', array(array(
+                'code' => '', 'name' => '',
+                'message' => 'No column to filter by is set'
+            )));
+        }
+
+        $conditions = array(
+            $this->_column . ' <=' => $this->_date,
+            'not' => array($this->_column => null),
+        );
+        if (!empty($this->_queue)) {
+            $conditions['Job.queue'] = $this->_queue;
+        }
+
+        $JobModel = ClassRegistry::init('Job');
+        $jobs = $JobModel->find('all', array('conditions' => $conditions));
+        foreach ($jobs as $job) {
+            switch ($this->_action) {
+                case 'unlock':
+                    $job['Job']['locked_at'] = null;
+                    $job['Job']['locked_by'] = null;
+                    $JobModel->save($job);
+                    if ($this->_save) $JobModel->save($job);
+                break;
+                case 'clean':
+                    if ($this->_save) $JobModel->delete($job['Job']['id']);
+                break;
+            }
+        }
+
+        $this->out(sprintf('%d %s%s', count($jobs), ($this->_save)? '':'not', " {$this->_action}ed\n"));
     }
 
 /**
@@ -153,7 +251,7 @@ The Worker Shell runs jobs created by the DJJob system.
 ---------------------------------------------------------------
 Usage: cake worker <command> <arg1> <arg2>...
 ---------------------------------------------------------------
-Params:
+Global Params:
     -connection <config>
         set db config <config>.
         default config: 'default'
@@ -162,10 +260,16 @@ Params:
         pdo name for connection <type>.
         default type: 'mysql'
 
+    -debug <boolean>
+        set debug level dynamically for running jobs
+        default value: {$this->_debug}
+        valid values: true, false, 0, 1, 2
+
     -queue <name>
         queue <name> to pull jobs from.
         default queue: {$this->_queue}
 
+Run Params:
     -count <number>
         run <number> of jobs to run before exiting (0 for unlimited).
         default count: {$this->_count}
@@ -178,18 +282,34 @@ Params:
         max <number> of retries for a given job.
         default retries: {$this->_max}
 
-    -debug <boolean>
-        set debug level dynamically for running jobs
-        default value: {$this->_debug}
-        valid values: true, false, 0, 1, 2
+Clean Params:
+    -action <string>
+        action to perform on cleanup task
+        default value: null
+
+    -date <string>
+        date offset
+        default value: {$this->_date}
+
+    -save <boolean>
+        allow cleanup to modify database
+        default value: {$this->_save}
+        valid values: true, false, 0, 1
 
 Commands:
 
     worker help
         shows this help message.
 
-    schema run
+    worker run
         runs jobs in the system
+
+    worker status
+        returns the status of a job queue
+
+    worker cleanup
+        returns the status of a job queue
+
 TEXT;
         $this->out($help);
         $this->_stop();
